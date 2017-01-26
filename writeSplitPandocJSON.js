f0238081e55886b6bea6726f7ab72cca543989ff
jan d <d_jan@ymail.com>
2017-01-26 22:54:15 +0100

    with command line args

--- writeSplitPandocJSON.js
+++ writeSplitPandocJSON.js
@@ -2,21 +2,17 @@
 Tool gets PandocJSON via stdin
 Splits it at 1st level headlines into chapter-sized Pandoc-JSONs
 Writes these Pandoc-JSONs to htmls
-
-TODO:
-- proper index page
-- proper error handling
-
+Example: pandoc -t json testdoc.md | node writeSplitPandocJSON.js
 MAYBE:
 - wrap the pandocJSON and the Pandoc-JSONs in an object which allows
   a cleaner passing(flow).of(conversions)?
--
-
 */
 var stdin  = require('get-stdin'),
 	pandoc = require('pandoc-filter'),
 	child_process = require('child_process'),
+	path = require('path'),
 	underscore = require('underscore'),
+	cli = require('cli'),
 	getSplitpoints, //function
 	splitPandocJSONFull, //function
 	findLinks, //function
@@ -25,7 +21,32 @@ var stdin  = require('get-stdin'),
 	addNavAdditionalMeta,//function
 	mergeMetadataToDoc;//function
 
+var config = {
+	default_indexTemplate:__dirname+"/"+"newhtmltemp.html",
+	default_chapterTemplate:__dirname+"/"+"newhtmltemp.html"
+};
+// Configure program and arguments
 
+
+var cliArguments = cli.parse({
+    indextemplate: [ false, 'index template', 'file', config.default_indexTemplate],          // -f, --file FILE   A file to process
+    chaptertemplate: [ false, 'chapter template', 'file', config.default_indexTemplate],                 // -t, --time TIME   An access time
+    indexpandoc: [ false, 'passed to pandoc', 'string', '' ],
+	chapterpandoc: [false,'passed to pandoc', 'string', '']
+});
+
+
+
+// var cliArguments= program.parse({ //can't do multiple arguments of same type
+// 	//'index-template':[false,'pandoc-template for indexpage','file',config.default_indexTemplate],
+// 	//'chapter-template':[false,'pandoc-template for chapterpages','file',config.default_chapterTemplate],
+// 	'index-pandoc-args':[false,'arguments passed to pandoc when creating index page','string',""],
+// 	'chapter-pandoc-args':[false,'arguments passed to pandoc when creating chapter page','string',""]
+// });
+
+
+
+// main function reading from stdin
 stdin().then(function(stringJSON){
 	console.log("gotstdin");
 	var pandocJSONFull =  JSON.parse(stringJSON);
@@ -41,28 +62,33 @@ stdin().then(function(stringJSON){
 	//var newDocsMetaWritten = addNavMeta(newDocs);
 
 	//new docs: array[n].doc array[n].doc array[n]filename
-	newDocsWithMeta.forEach(function(element, index, array){
+	//write Index
+	child_process.execSync('pandoc --from json -o index.html --standalone ' +
+		'--template='+cliArguments.indextemplate+
+		" "+cliArguments.indexpandoc,{input:JSON.stringify(newDocsWithMeta[0].doc)});
+
+	newDocsWithMeta.slice(1).forEach(function(element, index, array){ //for all except the 1st
 		var process = "pandoc";
 		var format = "html";
 		var filename = element.additionalMeta.filename;
+
 		if (filename.length === 0){
 			filename = "filename"+index;
 		}
 
 
 		//TODO: pandoc overwrites previously set arguments with later ones (at least -o notwrite.html - writethis.html). So one could pass user arguments, since I could overwrite them if need
-		//I can find the current directory of the script using __dirname which I could use for finding standard-templates and passing them to pandoc!
+		//I can find the current directory of the script using __dirname which I could use for finding standard-templates and passing them to pandoc. A direct reference like mytemplatehtml will always be evaluated in the context of the folder the script is executed in.
+		//this current folder I can get by __dirname; the folder I need for directing to possibel side files like  templates is ""
 
-		var pandocArguments = ["--from json","-o",filename, " --toc", "--standalone" , "--template=newhtmltemp.html"];
+		var pandocArguments = ["--from json","-o",filename, "--standalone" , '--template='+cliArguments.chaptertemplate,cliArguments.chapterpandoc];
 		var pandocify = JSON.stringify(element.doc);
 		console.log(pandocArguments.join(" "));
 		var whathappend = child_process.execSync('pandoc'+' '+pandocArguments.join(" "),{input:pandocify});
 		console.log(whathappend);
 	});
 
-	//write Index
 
-	child_process.execSync('pandoc --from json -o index.html --standalone --template=indexhtmltemp.html',{input:JSON.stringify(newDocsWithMeta[0].doc)});
 
 	//also, one index page:
 	//indexPage = generateIndexPage(newDocsWithMeta);
@@ -81,10 +107,16 @@ getSplitpoints = function(pandocJSONFull){
 	//  title ← the headline, stripped of non-word characters},
 	//…]
 
+	//TODO always add a first splitpoint with the title index.
+
 	var splitPoints = [];
-//array with objects {start:#, end#} for saving the node with the headline in the first, the node before the next headline in the second.
+
+	//TODO: Since there always should be a first page without chapter content as index page
+	//splitPoints.push({mainindex:0,title:"Index"});
+
+	//array with objects  for saving the array adress and the title.
 	pandocJSONFull.blocks.forEach(function(element, index, array){
-		if (element.t === "Header" && element.c[0] == 1){//headline first order aka chapter
+		if (element.t === "Header" && element.c[0] == 1){//is a headline && headline first is 1st order aka "chapter"
 			splitPoints.push({
 				"mainindex":index,
 				"title":pandoc. //NOTE: Do I ever need th title? For the filenames and links, the title/filename is generated in the "findLinks" function
@@ -95,10 +127,10 @@ getSplitpoints = function(pandocJSONFull){
 		}
 	});
 
-	//TODO: Since there always should be a first page without chapter content as index page, move this to the splitPandocJSONFull function, since there the copying of the metadata etc. happens, which is needed (an empty blocks still can get metadata); first page would always be index and we would most likely not generate stupid filenames from paragraph starts anymore in this case.
-	if(splitPoints[0].mainindex>0){ //text starts not with a headline. Add fake splitpoint, so that begin will be included.
-		splitPoints.unshift({mainindex:0,title:""});
-	}
+
+	// if(splitPoints[0].mainindex>0){ //text starts not with a headline. Add fake splitpoint, so that begin will be included.
+	// 	splitPoints.unshift({mainindex:0,title:""});
+	// }
 	return splitPoints;
 };
 
@@ -106,12 +138,19 @@ splitPandocJSONFull = function(pandocJSONFull, splitPoints){
 	//DOES: Splits the full PandocJSON in an array of chapter length PandocJSON Objects
 	//GETS:
 	//  pandocJSONFull: a PandocJSON of the full document
-	//  splitPoints: an array  ob ojjects, each like
+	//  splitPoints: an array  ob ojects, each like
 	// {mainindex ← the index of the headline in the pandoc JSON BLOCK array
 	//  title ← the headline of the chapter, stripped of non-word characters}
 	//
 	//RETURNS: Array with Pandoc-JSONs, each a chapter of the original document
 	var chapterArray = [];
+
+	//add index page to chapter array:
+	chapterArray.push({
+		"meta": JSON.parse(JSON.stringify(pandocJSONFull.meta)),
+		"blocks":pandocJSONFull.blocks.slice(0, splitPoints[0]),
+		"pandoc-api-version":JSON.parse(JSON.stringify(pandocJSONFull["pandoc-api-version"]))
+	});
 
 	//rewrite as Array.map
 	splitPoints.forEach(function(element, index, array){
@@ -129,8 +168,10 @@ splitPandocJSONFull = function(pandocJSONFull, splitPoints){
 		}
 
 		//get the blocks of a chapter from the pandocJSON
-		//NOTE: Array.slice does copy contained objects by reference. This is o.k. since we don’t want to have the same content twice in different documents.
-		//REMEMBER: Array.slice copies up to, but not including, endPoint!
+		//NOTE -Implementation: If a 1st level headline is the fist element, there may still be an element before this – an index page. So the start point will be 0, and the end point (the position of the next chapter start) will be 0, too, leading to an empty array in chapter blocks. Which is ok – the index page then has no own content BUT can still have metadata content like Title, author etc.
+		//NOTE -Javascriot: Array.slice does copy contained objects by reference. This is o.k. since we don’t want to have the same content twice in different documents.
+		//NOTE - Javascript: Array.slice copies up to, but not including, endPoint!
+
 		chapterBlocks = pandocJSONFull.blocks.slice(startPoint, endPoint);
 
 		//create a synthetic pandoc JSON object from the
@@ -159,8 +200,8 @@ findLinks = function (documentsArray){ //finds internal links
 			additionalMeta:{},
 			doc:null
 		};
-		rewrittenLinksArray[index].additionalMeta.filename = createDocumentFilename(documentsArray[index]);
-		rewrittenLinksArray[index].additionalMeta.naturalname = createDocumentNaturalName(documentsArray[index]);
+		rewrittenLinksArray[index].additionalMeta.filename = createDocumentFilename(documentsArray,index);
+		rewrittenLinksArray[index].additionalMeta.naturalname = createDocumentNaturalName(documentsArray,index);
 
 		rewrittenLinksArray[index].doc = pandoc.walk(
 				origDocument,
@@ -174,7 +215,7 @@ findLinks = function (documentsArray){ //finds internal links
 						} else if (targetIndex === index) {
 							return undefined; //internal link can stay. Target is in the same document
 						} else {
-							var filename = createDocumentFilename(documentsArray[targetIndex]);
+							var filename = createDocumentFilename(documentsArray,targetIndex);
 							var newValue = value;
 
 							//value[2][0] being the internal link,  something like #foobar
@@ -254,32 +295,39 @@ addNavAdditionalMeta = function(documentsArray){
 // called from the functions which do so.
 // ------------------------------------------------------------------------
 
-createDocumentFilename = function(singleDocumentArray){
-	//DOES: Create a chapter-title-based filename for a chapter
-	//GETS: a pandocJSON
+createDocumentFilename = function(documentArray, index){
+	//DOES: Create a chapter-title-based filename for a chapter, like myFirstChapter
+	//GETS: a array of pandocJSONs. Convention: 0th is index page and may have no content.
 	//RETURNS: a filename for the document made of the Pandoc-JSON.
-	var namestring;
-	if(singleDocumentArray.blocks[0].t === "Header"){ //if first block in array is a headline (should be the case)
-		namestring = singleDocumentArray.blocks[0].c[1][0];   // (c)ontent of the headline block (1)identifier array (0) identifier
+	var namestring = null;
+	var currentDocument = documentArray[index];
+
+	if(index === 0){ //0th is index page
+		namestring = "index";
+	} else if (currentDocument.blocks[0].t === "Header"){ //if first block in array is a headline (should be the case)
+		namestring = currentDocument.blocks[0].c[1][0];   // (c)ontent of the headline block (1)identifier array (0) identifier
 		//namestring = pandoc.stringify(singleDocumentArray.blocks[0].c).replace(/\W/g, '');//http://stackoverflow.com/questions/9364400/remove-not-alphanumeric-characters-from-string-having-trouble-with-the-char
 	}else{
-		namestring = pandoc.stringify(singleDocumentArray).slice(0,10).replace(/\W/g, ''); //this is rather inefficient to walk all the subtree (pandoc.stringify) to create a filename
+		namestring = pandoc.stringify(currentDocument).slice(0,10).replace(/\W/g, ''); //this is rather inefficient to walk all the subtree (pandoc.stringify) to create a filename
 	}
 	namestring = namestring+".html";
 	return namestring;
 };
 
-createDocumentNaturalName = function(singleDocumentArray){
+createDocumentNaturalName = function(documentArray, index){
 	//DOES: give a human readable name; if the first block is a headline, it is its stringified value.
 	//GETS: a pandocJSON
 	//RETURNS: a string
 
 	var namestring;
+	var currentDocument = documentArray[index];
 
-	if(singleDocumentArray.blocks[0].t === "Header"){
-		namestring = pandoc.stringify(singleDocumentArray.blocks[0]);
+	if(index === 0){ //0th is index page
+		namestring = "index";
+	} else if(currentDocument.blocks[0].t === "Header"){
+		namestring = pandoc.stringify(currentDocument.blocks[0]);
 	}else{
-		namestring = pandoc.stringify(singleDocumentArray.blocks[0]).slice(0,10); //since a paragraph could be rather long, we take the first 10 chars
+		namestring = pandoc.stringify(currentDocument.blocks[0]).slice(0,10); //since a paragraph could be rather long, we take the first 10 chars
 	}
 
 	return namestring;
